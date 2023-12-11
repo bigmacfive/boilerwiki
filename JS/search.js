0a3d6a8b4cc86a85e9b9fe2bb4e10bf02d183521
YoonjJun <108069900+YoonjJun@users.noreply.github.com>
2023-12-11 02:50:23 -0500

    Update search.js

--- JS/search.js
+++ JS/search.js
@@ -1,5 +1,21 @@
 function doSearch(text) {
     if (window.find(text)) {
         console.log(window.find(text));
+    if (!text) {
+        console.error("Hmmm, We didn't find anythig.");
+        return;
+    }
+}
+
+    if (typeof window.find !== 'function') {
+        console.error("mmm, We didn't find anythig.");
+        return;
+    }
+
+    var found = window.find(text);
+    if (found) {
+        console.log("text search: " + text);
+    } else {
+        console.log("mmm, We didn't find anythig. " + text);
     }
 }
